916a228e08b20f6fec7340dc6e9727cde13062e6
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-09-25 18:29:39 -0400

    remove js

--- sendbyemail.php
+++ sendbyemail.php
@@ -1,19 +1,8 @@
 <?php
-$dataJson = '{"recipient":"lucas.burlingham@mylca.net","link":"https://google.com","subject":"Google Search", "message": "Hey! I saw that you used bing..."}';
 
 if (isset($_POST["data"]) && !empty($_POST["data"])) {
     $message = $_POST["data"];
 }
-
-// $arr = json_decode($dataJson, true);
-$arr = json_decode($message, true);
-echo $arr["recipient"];
-echo "\n";
-echo $arr["link"];
-echo "\n";
-echo $arr["subject"];
-echo "\n";
-echo $arr["message"];
 
 $recipient = $_POST["recipient"];
 $subject = "JP New Tag: " . $_POST["subject"];
