4a7886626d32406783dc2dd605b1a3c7e156e468
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-09-25 17:10:26 -0400

    remove html

--- sendbyemail.php
+++ sendbyemail.php
@@ -1,12 +1,12 @@
 <?php
-// $dataJson = '{"recipient":"lucas.burlingham@mylca.net","link":"https://google.com","subject":"Google Search", "message": "Hey! I saw that you used bing..."}';
+$dataJson = '{"recipient":"lucas.burlingham@mylca.net","link":"https://google.com","subject":"Google Search", "message": "Hey! I saw that you used bing..."}';
 
 if (isset($_POST["data"]) && !empty($_POST["data"])) {
     $message = $_POST["data"];
 }
 
-$arr = json_decode($message, true);
-
+$arr = json_decode($dataJson, true);
+// $arr = json_decode($message, true);
 echo $arr["recipient"];
 echo "\n";
 echo $arr["link"];
@@ -15,9 +15,9 @@ echo $arr["subject"];
 echo "\n";
 echo $arr["message"];
 
-// $recipient = $_POST["recipient"];
-// $subject = "JP New Tag: " . $_POST["subject"];
-// $messageText = "The following was sent to you because whoever sent it thought that you may like it.\n \n" . $_POST["link"] . "Their message: \n" . $arr["message"] . "\n \n Sent With JPinit";
-// mail($recipient, $subject, $messageText);
+$recipient = $_POST["recipient"];
+$subject = "JP New Tag: " . $_POST["subject"];
+$messageText = "The following was sent to you because whoever sent it thought that you may like it.\n \n" . $_POST["link"] . "Their message: \n" . $arr["message"] . "\n \n Sent With JPinit";
+mail($recipient, $subject, $messageText);
 
 ?>
