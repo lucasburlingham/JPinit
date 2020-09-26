8d1e0a2ad52f406832e536a1b2e0d2f1cb5a8ac4
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-09-26 16:08:07 -0400

    Working (GET+REQUEST)

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
-if ($_POST["url"] || $_POST["email"] || $_POST['message']) {
-    $email = $_POST['email'];
-    $subject = "JPinit: " . $_POST['url'];
-    $message = "JPinit: \n" . $_POST['message'] . "URL \n " . $_POST['url'] . "\n\nThis message was sent by JPinit (https://jpinit.golca.org/). \n";
-    mail($email, $subject, $message);
+if ($_REQUEST["url"] || $_REQUEST["email"] || $_REQUEST['message']) {
+    $recipient = $_GET["email"];
+    $subject = "JP New Tag: " . $_GET["url"];
+    $messageText = "The following was sent to you because whoever sent it thought that you may like it.\n \n" . $_GET["url"] . "Their message: \n" . $_GET["message"] . "\n \n Sent With JPinit";
+    mail($recipient, $subject, $messageText);
     header("Location: https://golca.org/jpinit/");
     echo "Redirecting...";
     exit();
 } else {
     echo "Error in receiving... ";
-    echo "EMAIL: ". $_POST['email'];
-    echo "\n";
-    echo 'MESSAGE: ' . $_POST['message'];
-    echo "\n";
-    echo 'URL: ' . $_POST["url"];
+    echo "<b>";
+    echo "EMAIL: ". $_REQUEST['email'];
+    echo "<b>";
+    echo 'MESSAGE: ' . $_REQUEST['message'];
+    echo "<b>";
+    echo 'URL: ' . $_REQUEST["url"];
 }
 ?>
