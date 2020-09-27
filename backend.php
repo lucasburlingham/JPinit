ff3abbbc308e96eb502e01859517fc68ad81b650
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-09-27 13:05:56 -0400

    Working!

--- backend.php
+++ backend.php
@@ -1,10 +1,9 @@
 <?php
-require 'mailsettings.php';
 
 if ($_REQUEST["url"] || $_REQUEST["email"] || $_REQUEST['message']) {
     $recipient = $_GET["email"];
     $subject = "JP New Tag: " . $_GET["url"];
-    $messageText = "The following was sent to you because whoever sent it thought that you may like it.\n" . $_GET["url"] . "Their message: \n" . $_GET["message"] . "\n \n Sent With JPinit";
+    $messageText = "The following was sent to you because whoever sent it thought that you may like it.\n" . $_GET["url"] . "\n Their message: \n" . $_GET["message"] . "\n \n Sent With JPinit";
     mail($recipient, $subject, $messageText);
     header("Location: https://cloud.golca.org/jpinit/index.html");
     echo "Redirecting...";
